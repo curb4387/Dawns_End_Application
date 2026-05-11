94bb186f2666690f56e4ec821a31b133afb592e5
curb4387 <teenawtran@gmail.com>
2026-05-11 12:59:41 -0400

    Created extra class to hold main to start application from command line

---
+++ src/main/java/edu/dccc/dawnsend/GUIStarter.java
@@ -0,0 +1,11 @@
+package edu.dccc.dawnsend;
+
+import edu.dccc.dawnsend.DawnsEndFX;
+
+public class GUIStarter {
+
+    public static void main(final String[] args) {
+        DawnsEndFX.main(args);
+    }
+
+}
